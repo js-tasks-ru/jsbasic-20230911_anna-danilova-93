5fdfa2074443e05ef45d655f95b7d824538fbe11
Dmitrieva-Anna <anna_danilova_93@mail.ru>
2023-09-19 16:53:54 +0300

    module 2. Completed 3 tasks

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,13 @@
+function getRealNumber(num){
+  return Number.isFinite(num) ? num : 0;
+}
 function sumSalary(salaries) {
-  // ваш код...
+  let sumResult = 0;
+  for (let key in salaries) {
+    const value = salaries[key];
+    if(typeof value === 'number'){
+      sumResult += getRealNumber(value)
+    }
+  }
+  return sumResult
 }
