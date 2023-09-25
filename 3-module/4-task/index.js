65d127aacc4582732665c7bb6efe1e4ef778db65
Dmitrieva-Anna <anna_danilova_93@mail.ru>
2023-09-25 13:39:26 +0300

    module 3. Task 4. Fixed pull request

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,7 +1,15 @@
+// const showSalary = (users, ages) => {
+//   const filterUsers = users.filter(({age}) => age <= ages);
+//   return filterUsers.reduce((result, {name,balance}, index, arr) => {
+//     result += `${name}, ${balance}${index !== arr.length - 1 ? '\n' : ''}`
+//     return result
+//   }, '')
+// }
+
 const showSalary = (users, ages) => {
-  const filterUsers = users.filter(({age}) => age <= ages);
-  return filterUsers.reduce((result, {name,balance}, index, arr) => {
-    result += `${name}, ${balance}${index !== arr.length - 1 ? '\n' : ''}`
-    return result
-  }, '')
+    const filterUsers = users.filter(({ age }) => age <= ages);
+    return filterUsers.map(({ name, balance }) => {
+        return `${name}, ${balance}`
+
+    }).join('\n')
 }
