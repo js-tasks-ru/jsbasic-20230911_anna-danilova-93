a83f072b96f4b5924c6f5500ea905878c1b206b4
Dmitrieva-Anna <anna_danilova_93@mail.ru>
2023-09-27 19:22:30 +0300

    module 4. Tasks 1-3.

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,3 +1,8 @@
-function makeDiagonalRed(table) {
-  // ваш код...
+const makeDiagonalRed = (table) => {
+  for (let i = 0; i <= table.rows.length; i++) {
+    const row = table.rows[i];
+    if (row?.cells) {
+      row.cells[i].style.backgroundColor = 'red';
+    }
+  }
 }
