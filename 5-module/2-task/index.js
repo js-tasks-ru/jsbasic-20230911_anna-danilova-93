6d56c3793f90da5c68e480e112a5387f040423ee
Dmitrieva-Anna <anna_danilova_93@mail.ru>
2023-10-03 20:42:00 +0300

    module 5. Tasks 1-3.

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,10 @@
-function toggleText() {
-  // ваш код...
+const toggleText = () => {
+  const textHid = document.querySelector('#text');
+  const btnOffOn = document.querySelector('.toggle-text-button');
+
+  const actionHidden = () => textHid.hidden = !textHid.hidden
+
+  btnOffOn.addEventListener( 'click' , actionHidden);
+
+  return () => btnOffOn.removeEventListener( 'click' , actionHidden)
 }
