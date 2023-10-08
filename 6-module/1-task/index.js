c654408dd5e5b5bea6814322b45b35d73ee05a9a
Dmitrieva-Anna <anna_danilova_93@mail.ru>
2023-10-08 23:47:04 +0300

    module 6. Tasks completed.

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -12,7 +12,48 @@
  *      }
  *
  */
+import createElement from '../../assets/lib/create-element.js';
 export default class UserTable {
+  elem = null;
+  rows = [];
+  btn = null;
+
   constructor(rows) {
+    this.rows = rows;
+    this.btn = `<td><button>X</button></td>`;
+    this.render();
+  }
+  generateTable() {
+    return `
+      <table id="module-6-task-1">
+        <tbody>
+        ${this.rows.map((row, index) => `
+        <tr id='row-${index}'>
+            ${Object.values(row).map(value => `<td>${value}</td>`)}
+            ${this.btn}
+        </tr>`).join('')}
+        </tbody>
+      </table>
+    `
+  }
+
+  render() {
+    const temp = createElement(this.generateTable());
+
+   const btnList = temp.querySelectorAll('button'); // 4 btn
+
+    btnList.forEach((b, i) => b.addEventListener('click', () => this.delRow(i)))
+
+    this.elem = temp;
+  }
+
+  delRow (position) {
+    console.log('CLICK', position)
+    const sel = `#${position.toString()}`
+    const row = document.querySelector(`#row-${position}`);
+    row.remove();
   }
 }
+
+
+
